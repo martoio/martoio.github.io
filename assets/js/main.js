76d05d185c473ede85310efa850c1c66dd3da051
martoio <martoio@yahoo.com>
2016-02-12 21:23:06 -0500

    CSS + JS + Slider initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+	var images = $('.slider-image').toArray();
+	var activeImage = 0;
+	var numImages = $('.slider-image').length;
+	var buttonsWidth = 15;
+	var buttonsMargin = 24;
+	var sliderWidth = $('#slider').width();
+	var buttonsLeftPos = (sliderWidth - numImages*(buttonsMargin+buttonsWidth))/2;
+	
+	for (var i = 0; i < numImages; i++) {
+		if(images[i].classList.contains('active')) {
+			$('<div class="slider-button active" position="'+i+'"></div>').appendTo($('.slider-buttons'));
+			activeImage = i;
+		} else {
+			$('<div class="slider-button" position="'+i+'"></div>').appendTo($('.slider-buttons'));
+		}
+		console.log(images[i].classList.contains('active'));	
+		
+	};
+	
+	$('.slider-buttons').css({"left": buttonsLeftPos, "width": numImages*(buttonsWidth+buttonsMargin)-buttonsMargin});
+
+	
+
+
+});
